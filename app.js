fb1fece16e3bda70d893c97829ef429ff543df4f
Carletto <rcarletto@gmail.com>
2021-01-21 00:37:52 -0300

    alteração de cors

--- app.js
+++ app.js
@@ -1,31 +1,32 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const morgan = require('morgan');
+const morgan = require("morgan");
 
-const rotaProdutos = require('./routes/produtos');  
-const rotaMensagens = require('./routes/mensagens');
+const rotaProdutos = require("./routes/produtos");
+const rotaMensagens = require("./routes/mensagens");
 
+app.use(express.json());
+const corsOptions = { methods: "GET,PUT,PATCH,POST,DELETE" };
+app.use(cors(corsOptions));
 
-
-app.use(morgan('dev'));
-app.use('/produtos', rotaProdutos);
-app.use('/mensagens', rotaMensagens);
+app.use(morgan("dev"));
+app.use("/produtos", rotaProdutos);
+app.use("/mensagens", rotaMensagens);
 
 app.use((req, res, next) => {
-    const erro = new Error('invalid path');
-    erro.status = 404;
-    next(erro);
-
-}); 
-
-app.use((error, req, res, next)  => {
-    res.status(error.status || 500);
-    return res.send( {
-        erro: {
-            mensagem: error.message
-        }
-    });
-
+  const erro = new Error("invalid path");
+  erro.status = 404;
+  next(erro);
 });
 
-module.exports = app; 
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  return res.send({
+    erro: {
+      mensagem: error.message,
+    },
+  });
+});
+
+module.exports = app;
--- routes/mensagens.js
+++ routes/mensagens.js
@@ -1,38 +1,41 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-
 // retorna todos os mensagens
-router.get('/', (req, res, next) => {
-    res.status(200).send({
-        mensagem: "usando o get rota de mensagens"
-    });
-
+router.get("/", (req, res, next) => {
+  res.status(200).send({
+    mensagem: "usando o get rota de mensagens",
+  });
 });
 
 // insere um mensagem
-router.post('/', (req, res, next) => {
-    res.status(201).send({
-        mensagem: "mensagem criada"
-    });
+router.post("/", (req, res, next) => {
+  const id_msg = Math.floor(Math.random() * 1000) + 1;
+  const conteudo = req.body.content;
+
+  res.status(201).send({
+    mensagem: {
+      id_msg: id_msg,
+      conteudo: conteudo,
+    },
+  });
 });
 
 // get por id mensagem
-router.get('/:id_pedido', (req, res, next) => {
-    const id = req.params.id_pedido;
+router.get("/:id_pedido", (req, res, next) => {
+  const id = req.params.id_pedido;
 
-    res.status(200).send({
-        mensagem: "detalhes da mensagem",
-        id: id
-    });
-
+  res.status(200).send({
+    mensagem: "detalhes da mensagem",
+    id: id,
+  });
 });
 
 // deleta mensagem
-router.patch('/', (req, res, next) => {
-    res.status(201).send({
-        mensagem: "mensagem deletada"
-    });
+router.patch("/", (req, res, next) => {
+  res.status(201).send({
+    mensagem: "mensagem deletada",
+  });
 });
 
 module.exports = router;
